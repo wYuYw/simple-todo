62ae2e13715522960b8b45704865cfd84079a564
wYuYw <Luchsoa@gmail.com>
2022-05-04 15:51:53 +0900

    build todo and todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.scss';
 import InputField from './components/InputField/InputField';
+import TodoList from './components/TodoList/TodoList';
 import { Todo } from './models/models';
 
 const App: React.FC = () => {
@@ -20,6 +21,7 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">Todos</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './InputField.scss';
 
 interface Props {
@@ -8,9 +8,16 @@ interface Props {
 }
 
 const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input" onSubmit={handleAdd}>
+    <form className="input" onSubmit={(e) => {
+      handleAdd(e)
+      inputRef.current?.blur();
+    }}>
       <input
+        ref = {inputRef}
         type="input"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
