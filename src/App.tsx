136526ab8314d3032dab31b3409ceb302a5e8dce
wYuYw <Luchsoa@gmail.com>
2022-05-04 13:09:15 +0900

    create InputField

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import InputField from './components/InputField';
+import InputField from './components/InputField/InputField';
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+
+  console.log(todo);
+
   return (
     <div className="App">
       <span className='heading'>Todos</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
 };
